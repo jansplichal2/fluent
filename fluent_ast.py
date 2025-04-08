0bebc5196855befe05147ff804bfeb5199cf529a
Jan Splichal <jan.splichal@finshape.com>
2025-04-08 19:57:31 +0200

    Adding code

---
+++ fluent_ast.py
@@ -0,0 +1,95 @@
+from dataclasses import dataclass
+from typing import List, Optional, Union
+
+# === Expressions ===
+
+
+@dataclass
+class Expr:
+    pass
+
+
+@dataclass
+class Number(Expr):
+    value: int
+
+
+@dataclass
+class String(Expr):
+    value: str
+
+
+@dataclass
+class Var(Expr):
+    name: str
+
+
+@dataclass
+class Binary(Expr):
+    left: Expr
+    op: str
+    right: Expr
+
+
+@dataclass
+class Call(Expr):
+    func: str
+    args: List[Expr]
+
+
+@dataclass
+class IfExpr(Expr):
+    condition: Expr
+    then_branch: List['Stmt']
+    else_branch: Optional[List['Stmt']]
+
+
+@dataclass
+class MatchCase:
+    pattern: Union[int, str, "_"]
+    expr: Expr
+
+
+@dataclass
+class MatchExpr(Expr):
+    matched_expr: Expr
+    cases: List[MatchCase]
+
+
+@dataclass
+class ListLiteral(Expr):
+    start: Expr
+    end: Expr
+
+# === Statements ===
+
+
+@dataclass
+class Stmt:
+    pass
+
+
+@dataclass
+class LetStmt(Stmt):
+    name: str
+    type_annotation: Optional[str]
+    value: Expr
+
+
+@dataclass
+class FnParam:
+    name: str
+    type_annotation: str
+
+
+@dataclass
+class FnDecl(Stmt):
+    name: str
+    params: List[FnParam]
+    return_type: Optional[str]
+    body: List[Stmt]
+
+
+@dataclass
+class ExprStmt(Stmt):
+    expr: Expr
---
+++ parser.py
@@ -0,0 +1,61 @@
+from fluent_ast import LetStmt, Number
+from lexer import Lexer, TokenType, Token
+
+
+class Parser:
+    def __init__(self, source: str):
+        self.tokens = Lexer(source).tokenize()
+        self.pos = 0
+
+    def peek(self):
+        return self.tokens[self.pos]
+
+    def advance(self):
+        tok = self.tokens[self.pos]
+        self.pos += 1
+        return tok
+
+    def expect(self, type_):
+        tok = self.advance()
+        if tok.type != type_:
+            raise SyntaxError(f"Expected {type_}, got {tok.type} at line {tok.line + 1}")
+        return tok
+
+    def parse(self):
+        stmts = []
+        while self.peek().type != TokenType.EOF:
+            stmts.append(self.parse_stmt())
+        return stmts
+
+    def parse_stmt(self):
+        tok = self.peek()
+        if tok.type == TokenType.LET:
+            return self.parse_let()
+        else:
+            raise SyntaxError(f"Unexpected token: {tok.type} at line {tok.line + 1}")
+
+    def parse_let(self):
+        self.expect(TokenType.LET)
+        name_tok = self.expect(TokenType.IDENT)
+        self.expect(TokenType.ASSIGN)
+        value = self.parse_expr()
+        return LetStmt(name=name_tok.value, type_annotation=None, value=value)
+
+    def parse_expr(self):
+        tok = self.peek()
+        if tok.type == TokenType.NUMBER:
+            return self.parse_number()
+        else:
+            raise SyntaxError(f"Unsupported expression starting with {tok.type}")
+
+    def parse_number(self):
+        tok = self.expect(TokenType.NUMBER)
+        return Number(value=int(tok.value))
+
+
+if __name__ == '__main__':
+    source = 'let x = 42'
+    parser = Parser(source)
+    ast = parser.parse()
+    for stmt in ast:
+        print(stmt)
